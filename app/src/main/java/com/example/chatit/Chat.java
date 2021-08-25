0eaff77f772244c39cdf43b38a721d39ca68626e
Mustafa3011 <officewalamustafa@gmail.com>
2021-08-25 12:58:08 +0530

    camera image send

--- app/src/main/java/com/example/chatit/Chat.java
+++ app/src/main/java/com/example/chatit/Chat.java
@@ -4,22 +4,33 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Base64;
 import android.util.Log;
 import android.util.Pair;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 
 
 /**
@@ -50,8 +61,29 @@ public class Chat extends AppCompatActivity {
         });
         Button mediaSelect = findViewById(R.id.button3);
         mediaSelect.setOnClickListener(v -> {
-            Intent intent = new Intent(Chat.this,Media_Select.class);
-            startActivityForResult(intent,0);
+            LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = inflater.inflate(R.layout.media_select, null, false);
+            PopupWindow pw = new PopupWindow(view,600,400, true);
+            pw.showAsDropDown(mediaSelect, 0, 0, Gravity.TOP);
+            Button camera = view.findViewById(R.id.button4);
+            camera.setOnClickListener(u -> {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                    if (photoFile != null) {
+                        Uri photoURI = FileProvider.getUriForFile(this,
+                                "com.example.android.fileprovider",
+                                photoFile);
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(takePictureIntent, 113);
+                    }
+                }
+            });
         });
         EditText chatbox = findViewById(R.id.chatbox);
         Button send = findViewById(R.id.send);
@@ -76,6 +108,56 @@ public class Chat extends AppCompatActivity {
         chatsync.putExtra("password", this.getIntent().getStringExtra("password"));
         ServerConnect.enqueueWork(this, ServerConnect.class, 1000, chatsync);
         LocalBroadcastManager.getInstance(this).registerReceiver(reciever, new IntentFilter("com.example.chatit.CHATSYNC"));
+    }
+
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new Date().toString();
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+        currentPhotoPath = image.getPath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 113){
+            if (resultCode == RESULT_OK)
+            {
+                Uri uri = data.getData();
+                String path = currentPhotoPath;
+                try {
+                    FileInputStream fin = new FileInputStream(path);
+                    ByteArrayOutputStream bout = new ByteArrayOutputStream();
+                    int nRead;
+                    byte[] dat = new byte[16384];
+                    while ((nRead = fin.read(dat, 0, dat.length)) != -1) {
+                        bout.write(dat, 0, nRead);
+                    }
+                    dat = bout.toByteArray();
+                    Intent i = new Intent(this, ServerConnect.class);
+                    i.setAction("MESSAGE");
+                    i.putExtra("email", this.getIntent().getStringExtra("email"));
+                    i.putExtra("password", this.getIntent().getStringExtra("password"));
+                    i.putExtra("remail", this.getIntent().getStringExtra("remail"));
+                    i.putExtra("uname", this.getIntent().getStringExtra("uname"));
+                    i.putExtra("msg", Base64.encodeToString(dat, Base64.NO_WRAP));
+                    i.putExtra("type","IMG");
+                    ServerConnect.enqueueWork(this, ServerConnect.class, 1000, i);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        //TODO
     }
 
     @Override
@@ -108,8 +190,19 @@ public class Chat extends AppCompatActivity {
                 if (chat.second) ((LinearLayout.LayoutParams) txt.getLayoutParams()).gravity = Gravity.RIGHT;
                 txt.requestLayout();
             }
-            else if(chat.first.second.tp == Message.type.DOC){
-                //TODO
+            else if(chat.first.second.tp == Message.type.IMG){
+                ImageView im = new ImageView(this);
+                Bitmap bitmap = BitmapFactory.decodeByteArray(chat.first.second.data, 0, chat.first.second.data.length);
+                im.setImageBitmap(bitmap);
+                im.setPadding(10,10,10,10);
+                GradientDrawable shape = new GradientDrawable();
+                shape.setCornerRadius(16);
+                shape.setColor(Color.argb(255, 211, 247, 198));
+                im.setBackground(shape);
+                im.setLayoutParams(params);
+                chat_display.addView(im);
+                if (chat.second) ((LinearLayout.LayoutParams) im.getLayoutParams()).gravity = Gravity.RIGHT;
+                im.requestLayout();
             }
             else if(chat.first.second.tp == Message.type.AUDIO){
                 //TODO
@@ -117,7 +210,7 @@ public class Chat extends AppCompatActivity {
             else if(chat.first.second.tp == Message.type.VIDEO){
                 //TODO
             }
-            else if(chat.first.second.tp == Message.type.IMG){
+            else if(chat.first.second.tp == Message.type.DOC){
                 //TODO
             }
             else{
--- app/src/main/java/com/example/chatit/Media_Select.java
+++ app/src/main/java/com/example/chatit/Media_Select.java
@@ -15,19 +15,8 @@ public class Media_Select extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.media_select);
-        Button camera = findViewById(R.id.button4);
-        camera.setOnClickListener(v -> {
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(intent,113);
-        });
+
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 113){
-            finish();
-        }
-        //TODO
-    }
+
 }
